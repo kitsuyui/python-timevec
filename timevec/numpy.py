1671e17da9dce7a28163f69ad952d498e790473d
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-23 22:42:43 +0900

    Unify date conversion process in builtin_math and numpy.

--- timevec/numpy.py
+++ timevec/numpy.py
@@ -1,22 +1,22 @@
-import calendar
 import datetime
 
 import numpy as np
 import numpy.typing as npt
 
-from timevec.builtin_math import time_elapsed_ratio
+from timevec.util import (
+    day_range,
+    month_range,
+    time_elapsed_ratio,
+    week_range,
+    year_range,
+)
 
 
 def year_vec(
     dt: datetime.datetime, *, dtype: npt.DTypeLike = np.float64
 ) -> npt.NDArray:
     """Represent the elapsed time in the year as a vector"""
-    begin_of_year = datetime.datetime(
-        dt.year, month=1, day=1, hour=0, minute=0, second=0
-    )
-    end_of_year = datetime.datetime(
-        dt.year + 1, month=1, day=1, hour=0, minute=0, second=0
-    )
+    begin_of_year, end_of_year = year_range(dt)
     rate = time_elapsed_ratio(
         begin=begin_of_year,
         end=end_of_year,
@@ -29,13 +29,7 @@ def month_vec(
     dt: datetime.datetime, *, dtype: npt.DTypeLike = np.float64
 ) -> npt.NDArray:
     """Represent the elapsed time in the month as a vector"""
-    begin_of_month = datetime.datetime(
-        year=dt.year, month=dt.month, day=1, hour=0, minute=0, second=0
-    )
-    _, last_day = calendar.monthrange(dt.year, dt.month)
-    end_of_month = datetime.datetime(
-        year=dt.year, month=dt.month, day=last_day, hour=0, minute=0, second=0
-    ) + datetime.timedelta(days=1)
+    begin_of_month, end_of_month = month_range(dt)
     rate = time_elapsed_ratio(
         begin=begin_of_month,
         end=end_of_month,
@@ -48,22 +42,7 @@ def week_vec(
     dt: datetime.datetime, *, dtype: npt.DTypeLike = np.float64
 ) -> npt.NDArray:
     """Represent the elapsed time in the week as a vector"""
-    begin_of_week = datetime.datetime.min.replace(
-        year=dt.year,
-        month=dt.month,
-        day=dt.day,
-        hour=0,
-        minute=0,
-        second=0,
-    ) - datetime.timedelta(days=dt.weekday())
-    end_of_week = datetime.datetime.min.replace(
-        year=dt.year,
-        month=dt.month,
-        day=dt.day,
-        hour=0,
-        minute=0,
-        second=0,
-    ) + datetime.timedelta(days=7 - dt.weekday())
+    begin_of_week, end_of_week = week_range(dt)
     rate = time_elapsed_ratio(
         begin=begin_of_week,
         end=end_of_week,
@@ -76,22 +55,7 @@ def day_vec(
     dt: datetime.datetime, *, dtype: npt.DTypeLike = np.float64
 ) -> npt.NDArray:
     """Represent the elapsed time in the day as a vector"""
-    begin_of_day = datetime.datetime.min.replace(
-        year=dt.year,
-        month=dt.month,
-        day=dt.day,
-        hour=0,
-        minute=0,
-        second=0,
-    )
-    end_of_day = datetime.datetime.min.replace(
-        year=dt.year,
-        month=dt.month,
-        day=dt.day,
-        hour=0,
-        minute=0,
-        second=0,
-    ) + datetime.timedelta(days=1)
+    begin_of_day, end_of_day = day_range(dt)
     rate = time_elapsed_ratio(
         begin=begin_of_day,
         end=end_of_day,
