33375dc0d2422c33ee5ff1657ee572c766e7c4ab
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-02 20:07:58 +0900

    fix: make datetime64 conversion timezone-independent

--- tests/test_numpy_datetime64.py
+++ tests/test_numpy_datetime64.py
@@ -1,9 +1,26 @@
 import datetime
+import os
+import time
+from collections.abc import Iterator
 
 import numpy as np
+import pytest
 
 import timevec.numpy as tv
 import timevec.numpy_datetime64 as tv64
+
+
+@pytest.fixture
+def timezone() -> Iterator[None]:
+    old_tz = os.environ.get("TZ")
+    try:
+        yield
+    finally:
+        if old_tz is None:
+            os.environ.pop("TZ", None)
+        else:
+            os.environ["TZ"] = old_tz
+        time.tzset()
 
 
 def test_long_time_vec() -> None:
@@ -55,6 +72,27 @@ def test_multiple_datetime64() -> None:
     assert vec.shape == (3,)
     vec2 = np.frompyfunc(tv64.day_vec, 1, 1)(vec)
     assert np.stack(vec2, axis=0).shape == (3, 2)
-    vec3 = np.array([[dt64, dt64, dt64],
-                     [dt64, dt64, dt64]], dtype=np.datetime64)
+    vec3 = np.array(
+        [[dt64, dt64, dt64], [dt64, dt64, dt64]],
+        dtype=np.datetime64,
+    )
     assert vec3.shape == (2, 3)
+
+
+@pytest.mark.skipif(
+    not hasattr(time, "tzset"),
+    reason="timezone switching requires time.tzset()",
+)
+@pytest.mark.usefixtures("timezone")
+def test_datetime_to_datetime64_is_timezone_independent() -> None:
+    dt = datetime.datetime(2024, 1, 1, 12, 0, 0)
+
+    os.environ["TZ"] = "UTC"
+    time.tzset()
+    utc_result = tv64.datetime_to_datetime64(dt)
+
+    os.environ["TZ"] = "Asia/Tokyo"
+    time.tzset()
+    tokyo_result = tv64.datetime_to_datetime64(dt)
+
+    assert utc_result == tokyo_result == np.datetime64("2024-01-01T12:00:00")
